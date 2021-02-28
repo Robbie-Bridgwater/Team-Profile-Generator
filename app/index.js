a178b986761380646da8fe6ff2e75fce216b6fe4
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-02-28 13:58:40 +0000

    reformatted the layout to make the initialise function use less pairing

--- app/index.js
+++ app/index.js
@@ -1,26 +1,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const employeePrompts = () => {
-    return inquirer
-        .prompt([{
-            type: 'list',
-            name: 'empRole',
-            message: "What is the employee's role?",
-            choices: ['Manager', 'Engineer', 'Intern']
-        }, {
-            type: 'input',
-            name: 'empName',
-            message: "What is the employee's name?",
-        }, {
-            type: 'input',
-            name: 'empID',
-            message: "What is the employee's ID number?",
-        }, {
-            type: 'input',
-            name: 'empEmail',
-            message: "What is the employee's email?",
-        }, ])
-}
+const employeePrompt = [{
+    type: 'list',
+    name: 'empRole',
+    message: "What is the employee's role?",
+    choices: ['Manager', 'Engineer', 'Intern']
+}, {
+    type: 'input',
+    name: 'empName',
+    message: "What is the employee's name?",
+}, {
+    type: 'input',
+    name: 'empID',
+    message: "What is the employee's ID number?",
+}, {
+    type: 'input',
+    name: 'empEmail',
+    message: "What is the employee's email?",
+}, ]
 
-employeePrompts();
+const init = () => {
+    inquirer.prompt(employeePrompt)
+};
+
+init();
